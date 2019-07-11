4823bbea2a7f5efc2839506396b0b67dce2899aa
Joe Szafarowicz <joeszaf+github@gmail.com>
2019-07-11 17:10:17 -0400

    fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,39 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+// Joe's solution
+// function fizzBuzz(n) {
+//   for (let i = 1; i <= n; i++){
+//     let message = ""
+//     if (i % 3 == 0) {
+//       message += "fizz"
+//     }
+//     if (i % 5 == 0) {
+//       message += "buzz"
+//     }
+//
+//     if (message == "") {
+//       console.log(i)
+//     } else {
+//       console.log(message)
+//     }
+//   }
+// }
+
+// soultion
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    // check to see if the number is a multiple of both three and five
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz")
+    } else if (i % 3 === 0) {
+      console.log("fizz")
+    } else if (i % 5 === 0) {
+      console.log("buzz")
+    } else {
+      console.log(i)
+    }
+  }
+}
 
 module.exports = fizzBuzz;
