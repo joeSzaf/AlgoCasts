f6bf5ce9213feffc055b13af0272d2a4cef641aa
Joe Szafarowicz <joeszaf+github@gmail.com>
2019-08-09 11:37:52 -0400

    solutions for chunked

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,21 +8,55 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
+// Joe Solution
+
+// function chunk(array, size) {
+//   if (array.size === 0 || size === 0){
+//     return []
+//   }
+//
+//   let chunked = []
+//
+//   while (array.length > 0) {
+//     let currentChunk = []
+//     for (let i = 0; i < size; i++) {
+//       if (array.length > 0) {
+//         currentChunk.push(array.shift())
+//       }
+//     }
+//     chunked.push(currentChunk)
+//   }
+//
+//   return chunked
+// }
+
+// solution 1
+// function chunk(array, size) {
+//   const chunked = []
+//
+//   for (let element of array) {
+//     const last = chunked[chunked.length -1]
+//     console.log(last)
+//
+//     if (!last || last.length === size) {
+//       chunked.push([element])
+//     } else {
+//       last.push(element)
+//     }
+//   }
+//
+//   return chunked
+// }
+
+// solution 2
 function chunk(array, size) {
-  if (array.size === 0 || size === 0){
-    return []
-  }
+  const chunked = []
 
-  let chunked = []
+  let index = 0
 
-  while (array.length > 0) {
-    let currentChunk = []
-    for (let i = 0; i < size; i++) {
-      if (array.length > 0) {
-        currentChunk.push(array.shift())
-      }
-    }
-    chunked.push(currentChunk)
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size))
+    index += size
   }
 
   return chunked
